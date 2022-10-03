2ad11f82355cdbe7019a4797fd841305c65e571c
swc1996 <swc96@naver.com>
2022-10-03 16:22:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import "./App.css"; // 🔥 반드시 App.css 파일을 import 해줘야 합니다.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todos-container" id="todos">
+      안녕하세요
     </div>
   );
 }
